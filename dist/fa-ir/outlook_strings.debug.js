303ebf5912e59469a4a89dc476cbe462f1669530
Colby Sieber <colbys@microsoft.com>
2018-02-09 13:49:49 -0800

    Localized strings -- Moved void function definition back to bottom

---
+++ dist/fa-ir/outlook_strings.debug.js
@@ -0,0 +1,59 @@
+/* Version: 16.0.9106.1000 */
+
+Type.registerNamespace("_u");
+_u.ExtensibilityStrings = function()
+{
+};
+_u.ExtensibilityStrings.registerClass("_u.ExtensibilityStrings");
+_u.ExtensibilityStrings.l_NumberOfNotificationsExceeded_Text = "The notification couldn't be added because the notification limit has been reached.";
+_u.ExtensibilityStrings.l_PersistedNotificationArraySaveError_Text = "The API call you made failed because notifications couldn't be persisted.";
+_u.ExtensibilityStrings.l_InvalidTime_Text = "The input doesn't resolve to a valid time.";
+_u.ExtensibilityStrings.l_AttachmentUploadGeneralFailure_Text = "پیوست را نمی‌توان به این مورد اضافه کرد.";
+_u.ExtensibilityStrings.l_ParameterValueTooLongError_Text = "The value of the '{0}' parameter is too long. The maximum number of characters is '{1}'.";
+_u.ExtensibilityStrings.l_InvalidEndTime_Text = "تاریخ پایان نمی تواند قبل از تاریخ شروع باشد.";
+_u.ExtensibilityStrings.l_DeleteAttachmentDoesNotExist_Text = "پیوست را نمی‌توان حذف کرد زیرا پیوست در فهرست پیوست یافت نمی شود.";
+_u.ExtensibilityStrings.l_ExceededMaxNumberOfAttachments_Text = "نمی توان پیوستی را اضافه کرد زیرا این پیام از قبل حداکثر تعداد پیوست های مجاز را دارد";
+_u.ExtensibilityStrings.l_AttachmentDeletedBeforeUploadCompletes_Text = "کاربر قبل از تکمیل شدن بارگذاری پیوست، آن را حذف کرد.";
+_u.ExtensibilityStrings.l_AttachmentDeleteGeneralFailure_Text = "پیوست را نمی‌توان از این مورد حذف کرد.";
+_u.ExtensibilityStrings.l_InvalidParameterValueError_Text = "The value of the '{0}' parameter is invalid.";
+_u.ExtensibilityStrings.l_AttachmentExceededSize_Text = "پیوست را نمی‌توان اضافه کرد زیرا بیش از اندازه بزرگ است.";
+_u.ExtensibilityStrings.l_AttachmentNameTooLong_Text = "One or more of the attachment names is too long.";
+_u.ExtensibilityStrings.l_InvalidDate_Text = "ورودی به تاریخ معتبری تبدیل نمی شود.";
+_u.ExtensibilityStrings.l_Recurrence_Error_Properties_Invalid_Text = "The recurrence pattern isn't valid. Please check that the specified recurrence properties align with the recurrence type.";
+_u.ExtensibilityStrings.l_InternalProtocolError_Text = "خطای پروتکل داخلی: '{0}'.";
+_u.ExtensibilityStrings.l_MessageInDifferentStoreError_Text = "The EWS ID can't be retrieved because the message is saved in a different store.";
+_u.ExtensibilityStrings.l_NotificationKeyNotFound_Text = "There are no notifications with the provided key.";
+_u.ExtensibilityStrings.l_CannotPersistPropertyInUnsavedDraftError_Text = "Notifications can't be persisted in unsaved drafts. Save the item before you call this API.";
+_u.ExtensibilityStrings.l_HtmlSanitizationFailure_Text = "پاکسازی HTML ناموفق بود.";
+_u.ExtensibilityStrings.l_CursorPositionChanged_Text = "هنگامی که داده در حال وارد شدن بود، کاربر موقعیت مکان نما را تغییر داد.";
+_u.ExtensibilityStrings.l_DataWriteErrorName_Text = "خطای نوشتن داده";
+_u.ExtensibilityStrings.l_CannotAddAttachmentBeforeUpgrade_Text = "هنگامی که پاسخ کامل یا هدایت پیام در حال بازیابی از سرور است، نمی توان پیوستی را اضافه کرد.";
+_u.ExtensibilityStrings.l_InvalidEventDates_Text = "تاریخ پایان قبل از تاریخ آغاز است.";
+_u.ExtensibilityStrings.l_AttachmentErrorName_Text = "خطای پیوست";
+_u.ExtensibilityStrings.l_InvalidCommandIdError_Text = "The value of the parameter 'commandId' is invalid.";
+_u.ExtensibilityStrings.l_SaveError_Text = "Connection error occurred while trying to save the item on the server.";
+_u.ExtensibilityStrings.l_InvalidSelection_Text = "The selection is invalid.";
+_u.ExtensibilityStrings.l_InvalidActionType_Text = "The value of the parameter 'actionType' is invalid. The accepted value is \"showTaskPane\".";
+_u.ExtensibilityStrings.l_APICallFailedDueToItemChange_Text = "مورد انتخابی تغییر کرده است.";
+_u.ExtensibilityStrings.l_NullOrEmptyParameterError_Text = "The parameter '{0}' is mandatory and it can't be null or empty.";
+_u.ExtensibilityStrings.l_ActionsDefinitionWrongNotificationMessageError_Text = "Actions can't be defined for this notification message type.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeededForMethod_Text = "برای درخواست این روش به مجوز ویژه نیاز است: '{0}'.";
+_u.ExtensibilityStrings.l_CallSaveAsyncBeforeToken_Text = "The token can't be retrieved until the item is saved.";
+_u.ExtensibilityStrings.l_DisplayNameTooLong_Text = "یک یا چند نام نمایشی ارائه شده بیش از حد طولانی است.";
+_u.ExtensibilityStrings.l_InternalFormatError_Text = "خطای قالب داخلی رخ داد.";
+_u.ExtensibilityStrings.l_NoValidRecipientsProvided_Text = "گیرنده معتبری ارائه نشده است.";
+_u.ExtensibilityStrings.l_ParametersNotAsExpected_Text = "پارامترهای تعیین شد با قالب مورد انتظار مطابقت ندارند.";
+_u.ExtensibilityStrings.l_InvalidAttachmentId_Text = "شناسه پیوست نامعتبر بود.";
+_u.ExtensibilityStrings.l_OffsetNotfound_Text = "افستی برای این مهر زمان یافت نشد.";
+_u.ExtensibilityStrings.l_Recurrence_Error_Instance_SetAsync_Text = "A recurrence pattern can't be set for a single occurrence in a series.";
+_u.ExtensibilityStrings.l_ActionsDefinitionMultipleActionsError_Text = "Only a single action is currently supported.";
+_u.ExtensibilityStrings.l_AttachmentItemIdTooLong_Text = "One or more of the attachment IDs is too long.";
+_u.ExtensibilityStrings.l_EmailAddressTooLong_Text = "یک یا چند آدرس ایمیل ارائه شده بیش از حد طولانی است.";
+_u.ExtensibilityStrings.l_NumberOfRecipientsExceeded_Text = "تعداد کل گیرندگان در کارد نباید بیشتر از {0} باشد.";
+_u.ExtensibilityStrings.l_InvalidAttachmentPath_Text = "مسیر پیوست نامعتبر بود.";
+_u.ExtensibilityStrings.l_EwsRequestOversized_Text = "درخواست بیش از حد مجاز 1 مگابایت است. لطفاً درخواست EWS خود را اصلاح کنید.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeeded_Text = "برای دسترسی به اعضای محافظت شده JavaScript API برای Office به مجوز ویژه نیاز است.";
+_u.ExtensibilityStrings.l_AttachmentUrlTooLong_Text = "One or more of the attachment URLs is too long.";
+_u.ExtensibilityStrings.l_DuplicateNotificationKey_Text = "A notification with the provided key already exists.";
+_u.ExtensibilityStrings.l_PersistedNotificationArrayReadError_Text = "The API call you made failed because the persisted notifications couldn't be retrieved.";
+_u.ExtensibilityStrings.l_DisplayReplyFormHtmlBodyRequired_Text = "The 'htmlBody' is required."
---
+++ dist/te-in/outlook_strings.debug.js
@@ -0,0 +1,59 @@
+/* Version: 16.0.9106.1000 */
+
+Type.registerNamespace("_u");
+_u.ExtensibilityStrings = function()
+{
+};
+_u.ExtensibilityStrings.registerClass("_u.ExtensibilityStrings");
+_u.ExtensibilityStrings.l_InvalidTime_Text = "The input doesn't resolve to a valid time.";
+_u.ExtensibilityStrings.l_InvalidActionType_Text = "The value of the parameter 'actionType' is invalid. The accepted value is \"showTaskPane\".";
+_u.ExtensibilityStrings.l_EwsRequestOversized_Text = "అభ్యర్థన 1 MB పరిమాణ పరిమితిని మించి ఉంది. దయచేసి మీ EWS అభ్యర్థనను సవరించండి.";
+_u.ExtensibilityStrings.l_InternalFormatError_Text = "అంతర్గత ఆకృతి దోషం ఏర్పడింది.";
+_u.ExtensibilityStrings.l_OffsetNotfound_Text = "ఈ సమయ ముద్ర కోసం ఆఫ్‌సెట్‌ని కనుగొనలేకపోయాము.";
+_u.ExtensibilityStrings.l_AttachmentDeleteGeneralFailure_Text = "అంశం నుండి జోడింపుని తొలగించలేము.";
+_u.ExtensibilityStrings.l_CursorPositionChanged_Text = "డేటాను చొప్పిస్తున్నప్పుడు వినియోగదారు కర్సర్ యొక్క స్థానాన్ని మార్చారు.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeeded_Text = "Office కోసం JavaScript API యొక్క రక్షిత సభ్యుల ప్రాప్తికి పైస్థాయి అనుమతి అవసరం.";
+_u.ExtensibilityStrings.l_Recurrence_Error_Instance_SetAsync_Text = "A recurrence pattern can't be set for a single occurrence in a series.";
+_u.ExtensibilityStrings.l_CallSaveAsyncBeforeToken_Text = "The token can't be retrieved until the item is saved.";
+_u.ExtensibilityStrings.l_InvalidEventDates_Text = "ప్రారంభ తేదీ కంటే ముందు ముగింపు తేదీ ఉంది.";
+_u.ExtensibilityStrings.l_EmailAddressTooLong_Text = "పేర్కొన్న ఒకటి లేదా అంతకంటే ఎక్కువ ఇమెయిల్ చిరునామాలు చాలా పొడవుగా ఉన్నాయి.";
+_u.ExtensibilityStrings.l_CannotPersistPropertyInUnsavedDraftError_Text = "Notifications can't be persisted in unsaved drafts. Save the item before you call this API.";
+_u.ExtensibilityStrings.l_PersistedNotificationArrayReadError_Text = "The API call you made failed because the persisted notifications couldn't be retrieved.";
+_u.ExtensibilityStrings.l_DuplicateNotificationKey_Text = "A notification with the provided key already exists.";
+_u.ExtensibilityStrings.l_SaveError_Text = "Connection error occurred while trying to save the item on the server.";
+_u.ExtensibilityStrings.l_NoValidRecipientsProvided_Text = "చెల్లుబాటు అయ్యే గ్రహీతలు అందించబడలేదు.";
+_u.ExtensibilityStrings.l_InvalidEndTime_Text = "ప్రారంభ తేదీ కంటే ముగింపు తేదీ ముందు ఉండకూడదు.";
+_u.ExtensibilityStrings.l_DisplayNameTooLong_Text = "పేర్కొన్న ఒకటి లేదా అంతకంటే ఎక్కువ ప్రదర్శన పేర్లు చాలా పొడవుగా ఉన్నాయి.";
+_u.ExtensibilityStrings.l_AttachmentDeletedBeforeUploadCompletes_Text = "అప్‌లోడ్ పూర్తికాక ముందే వినియోగదారు జోడింపును తొలగించారు.";
+_u.ExtensibilityStrings.l_HtmlSanitizationFailure_Text = "HTML శాంటైజేషన్ విఫలమైంది.";
+_u.ExtensibilityStrings.l_DeleteAttachmentDoesNotExist_Text = "జోడింపు సూచికతో జోడింపు గుర్తించబడలేదు కాబట్టి జోడింపుని తొలగించలేము.";
+_u.ExtensibilityStrings.l_ParameterValueTooLongError_Text = "The value of the '{0}' parameter is too long. The maximum number of characters is '{1}'.";
+_u.ExtensibilityStrings.l_InvalidParameterValueError_Text = "The value of the '{0}' parameter is invalid.";
+_u.ExtensibilityStrings.l_ActionsDefinitionMultipleActionsError_Text = "Only a single action is currently supported.";
+_u.ExtensibilityStrings.l_ExceededMaxNumberOfAttachments_Text = "సందేశంలో ఇప్పటికే గరిష్ట సంఖ్యలో జోడింపులు ఉన్న కారణంగా జోడింపులను జోడించలేము";
+_u.ExtensibilityStrings.l_PersistedNotificationArraySaveError_Text = "The API call you made failed because notifications couldn't be persisted.";
+_u.ExtensibilityStrings.l_NumberOfRecipientsExceeded_Text = "ఫీల్డ్‌లో చేర్చిన మొత్తం గ్రహీతల సంఖ్య {0} కంటే ఎక్కువ ఉండకూడదు.";
+_u.ExtensibilityStrings.l_NullOrEmptyParameterError_Text = "The parameter '{0}' is mandatory and it can't be null or empty.";
+_u.ExtensibilityStrings.l_InvalidAttachmentPath_Text = "జోడింపు మార్గం చెల్లదు.";
+_u.ExtensibilityStrings.l_DisplayReplyFormHtmlBodyRequired_Text = "The 'htmlBody' is required.";
+_u.ExtensibilityStrings.l_InvalidDate_Text = "ఇన్‌పుట్ చెల్లుబాటు అయ్యే తేదీని పరిష్కరించడం లేదు.";
+_u.ExtensibilityStrings.l_ParametersNotAsExpected_Text = "ఆశించిన ఆకృతితో ఇచ్చిన పారామీటర్‌లు సరిపోలడం లేదు.";
+_u.ExtensibilityStrings.l_DataWriteErrorName_Text = "డేటా వ్రాయడంలో దోషం";
+_u.ExtensibilityStrings.l_Recurrence_Error_Properties_Invalid_Text = "The recurrence pattern isn't valid. Please check that the specified recurrence properties align with the recurrence type.";
+_u.ExtensibilityStrings.l_APICallFailedDueToItemChange_Text = "ఎంచుకోబడిన అంశం మార్చబడింది.";
+_u.ExtensibilityStrings.l_CannotAddAttachmentBeforeUpgrade_Text = "పూర్తి ప్రత్యుత్తరం లేదా ఫార్వార్డ్‌ని సర్వర్ నుండి పునరుద్ధరిస్తున్నప్పుడు జోడింపులను జోడించలేము.";
+_u.ExtensibilityStrings.l_AttachmentUploadGeneralFailure_Text = "జోడింపుని అంశానికి జోడించలేము.";
+_u.ExtensibilityStrings.l_MessageInDifferentStoreError_Text = "The EWS ID can't be retrieved because the message is saved in a different store.";
+_u.ExtensibilityStrings.l_NotificationKeyNotFound_Text = "There are no notifications with the provided key.";
+_u.ExtensibilityStrings.l_AttachmentErrorName_Text = "జోడింపు దోషం";
+_u.ExtensibilityStrings.l_AttachmentNameTooLong_Text = "One or more of the attachment names is too long.";
+_u.ExtensibilityStrings.l_InvalidSelection_Text = "The selection is invalid.";
+_u.ExtensibilityStrings.l_AttachmentUrlTooLong_Text = "One or more of the attachment URLs is too long.";
+_u.ExtensibilityStrings.l_ActionsDefinitionWrongNotificationMessageError_Text = "Actions can't be defined for this notification message type.";
+_u.ExtensibilityStrings.l_AttachmentItemIdTooLong_Text = "One or more of the attachment IDs is too long.";
+_u.ExtensibilityStrings.l_InvalidAttachmentId_Text = "జోడింపు ID చెల్లదు.";
+_u.ExtensibilityStrings.l_AttachmentExceededSize_Text = "జోడింపు చాలా పెద్దదిగా ఉండటం వలన దానిని జోడించలేము.";
+_u.ExtensibilityStrings.l_NumberOfNotificationsExceeded_Text = "The notification couldn't be added because the notification limit has been reached.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeededForMethod_Text = "ఇక్కడ ఉన్న పద్ధతికి కాల్ చేసేందుకు పైస్థాయి అనుమతి అవసరం: '{0}'.";
+_u.ExtensibilityStrings.l_InvalidCommandIdError_Text = "The value of the parameter 'commandId' is invalid.";
+_u.ExtensibilityStrings.l_InternalProtocolError_Text = "అంతర్గత ప్రోటోకాల్ దోషం: '{0}'."
