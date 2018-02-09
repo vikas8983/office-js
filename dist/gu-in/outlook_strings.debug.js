b0a3ef09991e19ceda743afd47d8917a8833c4c8
Debjani Saha <desaha@microsoft.com>
2018-02-09 23:19:52 +0530

    Localized strings -- __adhoc--release--local-enlistment--2018-02-09--11.18pm

---
+++ dist/gu-in/outlook_strings.debug.js
@@ -0,0 +1,59 @@
+/* Version: 16.0.9106.1000 */
+
+Type.registerNamespace("_u");
+_u.ExtensibilityStrings = function()
+{
+};
+_u.ExtensibilityStrings.registerClass("_u.ExtensibilityStrings");
+_u.ExtensibilityStrings.l_APICallFailedDueToItemChange_Text = "પસંદ કરેલી આઇટમ પરિવર્તિત કરી છે.";
+_u.ExtensibilityStrings.l_ActionsDefinitionMultipleActionsError_Text = "Only a single action is currently supported.";
+_u.ExtensibilityStrings.l_ActionsDefinitionWrongNotificationMessageError_Text = "Actions can't be defined for this notification message type.";
+_u.ExtensibilityStrings.l_AttachmentDeleteGeneralFailure_Text = "જોડાણને આઇટમમાંથી હટાવી શકાતું નથી.";
+_u.ExtensibilityStrings.l_AttachmentDeletedBeforeUploadCompletes_Text = "ઉપયોગકર્તાએ અપલોડ પૂર્ણ થયા પહેલાં જોડાણ કાઢી નાંખ્યું.";
+_u.ExtensibilityStrings.l_AttachmentErrorName_Text = "જોડાણની ભૂલ";
+_u.ExtensibilityStrings.l_AttachmentExceededSize_Text = "જોડાણ ઍડ કરી શકાયું નથી કારણ કે તે ખૂબ મોટું છે.";
+_u.ExtensibilityStrings.l_AttachmentItemIdTooLong_Text = "One or more of the attachment IDs is too long.";
+_u.ExtensibilityStrings.l_AttachmentNameTooLong_Text = "One or more of the attachment names is too long.";
+_u.ExtensibilityStrings.l_AttachmentUploadGeneralFailure_Text = "જોડાણને આઇટમમાં ઍડ કરી શકાતું નથી.";
+_u.ExtensibilityStrings.l_AttachmentUrlTooLong_Text = "One or more of the attachment URLs is too long.";
+_u.ExtensibilityStrings.l_CallSaveAsyncBeforeToken_Text = "The token can't be retrieved until the item is saved.";
+_u.ExtensibilityStrings.l_CannotAddAttachmentBeforeUpgrade_Text = "જ્યારે સર્વર તરફથી સંપૂર્ણ જવાબ અથવા ફોર્વર્ડ પુનઃપ્રાપ્ત થઈ રહ્યાં હોય ત્યારે જોડાણો ઍડ કરી શકાતા નથી.";
+_u.ExtensibilityStrings.l_CannotPersistPropertyInUnsavedDraftError_Text = "Notifications can't be persisted in unsaved drafts. Save the item before you call this API.";
+_u.ExtensibilityStrings.l_CursorPositionChanged_Text = "ડેટા સામેલ કરવામાં આવી રહ્યો હતો ત્યારે ઉપયોગકર્તાએ કર્સરનું સ્થાન બદલ્યું હતું.";
+_u.ExtensibilityStrings.l_DataWriteErrorName_Text = "ડેટા લેખનમાં ભૂલ";
+_u.ExtensibilityStrings.l_DeleteAttachmentDoesNotExist_Text = "જોડાણ હટાવી શકાતું નથી કારણ કે જોડાણ અનુક્રમણિકા ધરાવતું જોડાણ શોધી શકાતું નથી.";
+_u.ExtensibilityStrings.l_DisplayNameTooLong_Text = "પ્રદાન કરેલા પ્રદર્શન નામોમાં એક કે વધુ ખૂબ લાંબાં છે.";
+_u.ExtensibilityStrings.l_DisplayReplyFormHtmlBodyRequired_Text = "The 'htmlBody' is required.";
+_u.ExtensibilityStrings.l_DuplicateNotificationKey_Text = "A notification with the provided key already exists.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeededForMethod_Text = "પદ્ધતિને કૉલ કરવા માટે ઉચ્ચ અનુમતિ આવશ્યક છે: '{0}'.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeeded_Text = "JavaScript API for Office ના સંરક્ષિત સભ્યોને ઍક્સેસ કરવા માટે ઉચ્ચ અનુમતિ આવશ્યક છે.";
+_u.ExtensibilityStrings.l_EmailAddressTooLong_Text = "પ્રદાન કરેલા ઈમેલ સરનામામાં એક કે વધુ ખૂબ લાંબાં છે.";
+_u.ExtensibilityStrings.l_EwsRequestOversized_Text = "વિનંતી 1 MB કદ સીમાને વટાવે છે. કૃપયા તમારી EWS વિનંતી સંશોધિત કરો.";
+_u.ExtensibilityStrings.l_ExceededMaxNumberOfAttachments_Text = "જોડાણો ઍડ કરી શકાયા નથી કારણ કે સંદેશમાં પહેલેથી મહત્તમ સંખ્યાના જોડાણો છે";
+_u.ExtensibilityStrings.l_HtmlSanitizationFailure_Text = "HTML સેનીટાઇઝેશન નિષ્ફળ થયું.";
+_u.ExtensibilityStrings.l_InternalFormatError_Text = "એક આંતરિક સ્વરૂપ ભૂલ આવી હતી.";
+_u.ExtensibilityStrings.l_InternalProtocolError_Text = "આંતરિક પ્રોટોકૉલ ભૂલ: '{0}'.";
+_u.ExtensibilityStrings.l_InvalidActionType_Text = "The value of the parameter 'actionType' is invalid. The accepted value is \"showTaskPane\".";
+_u.ExtensibilityStrings.l_InvalidAttachmentId_Text = "જોડાણ ID અમાન્ય હતું.";
+_u.ExtensibilityStrings.l_InvalidAttachmentPath_Text = "જોડાણ પાથ અમાન્ય હતો.";
+_u.ExtensibilityStrings.l_InvalidCommandIdError_Text = "The value of the parameter 'commandId' is invalid.";
+_u.ExtensibilityStrings.l_InvalidDate_Text = "ઇનપુટને માન્ય તારીખ પર હલ કરી શકતો નથી.";
+_u.ExtensibilityStrings.l_InvalidEndTime_Text = "સમાપ્તિ સમય પ્રારંભ સમય પહેલાંનો હોઇ શકે નહીં.";
+_u.ExtensibilityStrings.l_InvalidEventDates_Text = "સમાપ્તિ તારીખ પ્રારંભ તારીખ પહેલાં આવે છે.";
+_u.ExtensibilityStrings.l_InvalidParameterValueError_Text = "The value of the '{0}' parameter is invalid.";
+_u.ExtensibilityStrings.l_InvalidSelection_Text = "The selection is invalid.";
+_u.ExtensibilityStrings.l_InvalidTime_Text = "The input doesn't resolve to a valid time.";
+_u.ExtensibilityStrings.l_MessageInDifferentStoreError_Text = "The EWS ID can't be retrieved because the message is saved in a different store.";
+_u.ExtensibilityStrings.l_NoValidRecipientsProvided_Text = "કોઈ માન્ય પ્રાપ્તકર્તાઓ પ્રદાન કરેલા ન હતાં.";
+_u.ExtensibilityStrings.l_NotificationKeyNotFound_Text = "There are no notifications with the provided key.";
+_u.ExtensibilityStrings.l_NullOrEmptyParameterError_Text = "The parameter '{0}' is mandatory and it can't be null or empty.";
+_u.ExtensibilityStrings.l_NumberOfNotificationsExceeded_Text = "The notification couldn't be added because the notification limit has been reached.";
+_u.ExtensibilityStrings.l_NumberOfRecipientsExceeded_Text = "ફીલ્ડમાં પ્રાપ્તકર્તાઓની કુલ સંખ્યા {0} ને વટાવી શકતી નથી."
+_u.ExtensibilityStrings.l_OffsetNotfound_Text = "આ ટાઇમ સ્ટેમ્પ માટે એક ઓફસેટ શોધી શકાયું નથી.";
+_u.ExtensibilityStrings.l_ParameterValueTooLongError_Text = "The value of the '{0}' parameter is too long. The maximum number of characters is '{1}'.";
+_u.ExtensibilityStrings.l_ParametersNotAsExpected_Text = "આપેલા પેરામીટર્સ અપેક્ષિત સ્વરૂપથી મેળ ખાતા નથી.";
+_u.ExtensibilityStrings.l_PersistedNotificationArrayReadError_Text = "The API call you made failed because the persisted notifications couldn't be retrieved.";
+_u.ExtensibilityStrings.l_PersistedNotificationArraySaveError_Text = "The API call you made failed because notifications couldn't be persisted.";
+_u.ExtensibilityStrings.l_Recurrence_Error_Instance_SetAsync_Text = "A recurrence pattern can't be set for a single occurrence in a series.";
+_u.ExtensibilityStrings.l_Recurrence_Error_Properties_Invalid_Text = "The recurrence pattern isn't valid. Please check that the specified recurrence properties align with the recurrence type.";
+_u.ExtensibilityStrings.l_SaveError_Text = "Connection error occurred while trying to save the item on the server.";
---
+++ dist/ta-in/outlook_strings.debug.js
@@ -0,0 +1,59 @@
+/* Version: 16.0.9106.1000 */
+
+Type.registerNamespace("_u");
+_u.ExtensibilityStrings = function()
+{
+};
+_u.ExtensibilityStrings.registerClass("_u.ExtensibilityStrings");
+_u.ExtensibilityStrings.l_APICallFailedDueToItemChange_Text = "தேர்ந்தெடுத்த உருப்படி மாற்றப்பட்டுள்ளது.";
+_u.ExtensibilityStrings.l_ActionsDefinitionMultipleActionsError_Text = "Only a single action is currently supported.";
+_u.ExtensibilityStrings.l_ActionsDefinitionWrongNotificationMessageError_Text = "Actions can't be defined for this notification message type.";
+_u.ExtensibilityStrings.l_AttachmentDeleteGeneralFailure_Text = "இணைப்பை, இந்த உருப்படியிலிருந்து அகற்ற முடியாது.";
+_u.ExtensibilityStrings.l_AttachmentDeletedBeforeUploadCompletes_Text = "பதிவேற்றம் முடிவடையும் முன்பே, பயனர் இணைப்பை அகற்றி விட்டார்.";
+_u.ExtensibilityStrings.l_AttachmentErrorName_Text = "இணைப்பில் பிழை";
+_u.ExtensibilityStrings.l_AttachmentExceededSize_Text = "இணைப்பு மிகவும் பெரியதாக இருப்பதால், அதைச் சேர்க்க முடியாது.";
+_u.ExtensibilityStrings.l_AttachmentItemIdTooLong_Text = "One or more of the attachment IDs is too long."
+_u.ExtensibilityStrings.l_AttachmentNameTooLong_Text = "One or more of the attachment names is too long.";
+_u.ExtensibilityStrings.l_AttachmentUploadGeneralFailure_Text = "இணைப்பை, இந்த உருப்படியுடன் சேர்க்க முடியாது.";
+_u.ExtensibilityStrings.l_AttachmentUrlTooLong_Text = "One or more of the attachment URLs is too long.";
+_u.ExtensibilityStrings.l_CallSaveAsyncBeforeToken_Text = "The token can't be retrieved until the item is saved.";
+_u.ExtensibilityStrings.l_CannotAddAttachmentBeforeUpgrade_Text = "சேவையகத்திலிருந்து, முழுமையான பதில் அல்லது முன்னனுப்புதல் எடுக்கப்படும்போது, இணைப்புகளைச் சேர்க்க முடியாது.";
+_u.ExtensibilityStrings.l_CannotPersistPropertyInUnsavedDraftError_Text = "Notifications can't be persisted in unsaved drafts. Save the item before you call this API.";
+_u.ExtensibilityStrings.l_CursorPositionChanged_Text = "தரவு செருகப்படும்போது, பயனர் கர்சரின் நிலையை மாற்றி விட்டார்.";
+_u.ExtensibilityStrings.l_DataWriteErrorName_Text = "தரவு எழுதுதல் பிழை";
+_u.ExtensibilityStrings.l_DeleteAttachmentDoesNotExist_Text = "இணைப்பை நீக்க முடியாது, ஏனெனில் இணைப்பு அட்டவணையுடன் கூடிய இணைப்பைக் கண்டறிய முடியவில்லை.";
+_u.ExtensibilityStrings.l_DisplayNameTooLong_Text = "வழங்கப்பட்ட காட்சி பெயர்களில், ஒன்று அல்லது மேற்பட்டவை நீளமாக உள்ளன.";
+_u.ExtensibilityStrings.l_DisplayReplyFormHtmlBodyRequired_Text = "The 'htmlBody' is required.";
+_u.ExtensibilityStrings.l_DuplicateNotificationKey_Text = "A notification with the provided key already exists.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeededForMethod_Text = "பின்வரும் முறையை அழைக்க, மேம்பட்ட அனுமதி தேவை: '{0}'.";
+_u.ExtensibilityStrings.l_ElevatedPermissionNeeded_Text = "Office-க்கான JavaScript API இன் பாதுகாக்கப்பட்ட உறுப்பினர்களை அணுகுவதற்கு, இன்னும் மேம்பட்ட அனுமதி தேவை.";
+_u.ExtensibilityStrings.l_EmailAddressTooLong_Text = "வழங்கப்பட்ட மின்னஞ்சல் முகவரிகளில், ஒன்று அல்லது மேற்பட்டவை நீளமாக உள்ளன.";
+_u.ExtensibilityStrings.l_EwsRequestOversized_Text = "கோரிக்கையானது, 1 MB அளவு வரம்பை மீறுகிறது. உங்கள் EWS கோரிக்கையை மாற்றவும்.";
+_u.ExtensibilityStrings.l_ExceededMaxNumberOfAttachments_Text = "செய்தியில் ஏற்கனவே அதிகபட்ச இணைப்புகள் இருப்பதால், இணைப்புகளைச் சேர்க்க முடியாது";
+_u.ExtensibilityStrings.l_HtmlSanitizationFailure_Text = "HTML சுத்திகரிப்பு தோல்வியுற்றது.";
+_u.ExtensibilityStrings.l_InternalFormatError_Text = "உள்ளக வடிவமைப்பு பிழை ஏற்பட்டது.";
+_u.ExtensibilityStrings.l_InternalProtocolError_Text = "உள்ளக நெறிமுறை பிழை: '{0}'.";
+_u.ExtensibilityStrings.l_InvalidActionType_Text = "The value of the parameter 'actionType' is invalid. The accepted value is \"showTaskPane\".";
+_u.ExtensibilityStrings.l_InvalidAttachmentId_Text = "இணைப்பு ஐடி செல்லுபடியாகாதது.";
+_u.ExtensibilityStrings.l_InvalidAttachmentPath_Text = "இணைப்பின் பாதை செல்லுபடியாகாதது.";
+_u.ExtensibilityStrings.l_InvalidCommandIdError_Text = "The value of the parameter 'commandId' is invalid.";
+_u.ExtensibilityStrings.l_InvalidDate_Text = "இந்த உள்ளீடு, சரியான தேதி அல்ல.";
+_u.ExtensibilityStrings.l_InvalidEndTime_Text = "முடியும் நேரம், தொடக்க நேரத்திற்கு முன்னதாக இருக்க முடியாது.";
+_u.ExtensibilityStrings.l_InvalidEventDates_Text = "இறுதி தேதி, தொடக்கத் தேதிக்கு முன்னதாக உள்ளது.";
+_u.ExtensibilityStrings.l_InvalidParameterValueError_Text = "The value of the '{0}' parameter is invalid.";
+_u.ExtensibilityStrings.l_InvalidSelection_Text = "The selection is invalid.";
+_u.ExtensibilityStrings.l_InvalidTime_Text = "The input doesn't resolve to a valid time.";
+_u.ExtensibilityStrings.l_MessageInDifferentStoreError_Text = "The EWS ID can't be retrieved because the message is saved in a different store.";
+_u.ExtensibilityStrings.l_NoValidRecipientsProvided_Text = "செல்லுபடியாகும் பெறுநர்கள் இல்லை.";
+_u.ExtensibilityStrings.l_NotificationKeyNotFound_Text = "There are no notifications with the provided key.";
+_u.ExtensibilityStrings.l_NullOrEmptyParameterError_Text = "The parameter '{0}' is mandatory and it can't be null or empty.";
+_u.ExtensibilityStrings.l_NumberOfNotificationsExceeded_Text = "The notification couldn't be added because the notification limit has been reached.";
+_u.ExtensibilityStrings.l_NumberOfRecipientsExceeded_Text = "ஒரு புலத்தில் உள்ள மொத்த பெறுநர்களின் எண்ணிக்கை, {0}-க்கு மேல் இருக்கக் கூடாது.";
+_u.ExtensibilityStrings.l_OffsetNotfound_Text = "இந்த நேர முத்திரைக்கான ஆஃப்செட் கிடைக்கவில்லை.";
+_u.ExtensibilityStrings.l_ParameterValueTooLongError_Text = "The value of the '{0}' parameter is too long. The maximum number of characters is '{1}'.";
+_u.ExtensibilityStrings.l_ParametersNotAsExpected_Text = "தரப்பட்ட அளவருக்கள், எதிர்பார்க்கப்படும் வடிவமைப்புடன் பொருந்தவில்லை.";
+_u.ExtensibilityStrings.l_PersistedNotificationArrayReadError_Text = "The API call you made failed because the persisted notifications couldn't be retrieved.";
+_u.ExtensibilityStrings.l_PersistedNotificationArraySaveError_Text = "The API call you made failed because notifications couldn't be persisted.";
+_u.ExtensibilityStrings.l_Recurrence_Error_Instance_SetAsync_Text = "A recurrence pattern can't be set for a single occurrence in a series.";
+_u.ExtensibilityStrings.l_Recurrence_Error_Properties_Invalid_Text = "The recurrence pattern isn't valid. Please check that the specified recurrence properties align with the recurrence type.";
+_u.ExtensibilityStrings.l_SaveError_Text = "Connection error occurred while trying to save the item on the server.";
